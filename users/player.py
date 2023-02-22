19160fb3bb8f5d2ada7407dab3b9b40ba08d1dd0
Zach <zach.jilani1@gmail.com>
2023-02-22 12:53:56 -0800

    state testing

--- users/player.py
+++ users/player.py
@@ -9,7 +9,6 @@ class Player:
   def __init__(self, number, score=0):
     self.number = number
     self.score = score
-    self.state = "start"
     self.questions_asked = []
 
 
@@ -17,17 +16,18 @@ class Player:
     self.questions_asked.append(question)
 
   def questions(self, input):
+    state = 'start'
     out = []
-    for next_question in QUESTIONS['start']['next_state']:
+    for next_question in QUESTIONS[state]['next_state']:
       if input.lower() == next_question['input'].lower():
-        self.state = next_question['next_state']
+        state = next_question['next_state']
         if 'point' in next_question['point']:
           self.score += next_question['point']
           out.append(self.score)
     while True:
-      out.append(QUESTIONS[self.state]['content'])
-      if 'next_state' not in QUESTIONS[self.state]:
+      out.append(QUESTIONS[state]['content'])
+      if 'next_state' not in QUESTIONS[state]:
         break
-      self.state = QUESTIONS[self.state]['next_state']
+      state = QUESTIONS[state]['next_state']
 
     return out
