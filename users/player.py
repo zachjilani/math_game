f793748bd8f75aeb2b0e1425bfd7fc71632eedc2
Zach <zach.jilani1@gmail.com>
2023-02-23 15:26:40 -0800

    changing state creation

--- users/player.py
+++ users/player.py
@@ -4,13 +4,17 @@ QUESTIONS = {}
 with open('RevisedQuestions.json', 'r') as myfile:
   QUESTIONS = json.loads(myfile.read())
 
-state = 'start'
 
 class Player:
   def __init__(self, number, score=0):
     self.number = number
     self.score = score
     self.questions_asked = []
+    self.state = 'start'
+
+  def info(self):
+    print(self.state)
+    print(self.score)
 
 
   def save_player(self, question):
@@ -18,21 +22,21 @@ class Player:
 
   def questions(self, input):
     out = []
-    for next_question in QUESTIONS[state]['next_question']:
-      print(f'before state change: {state}')
+    for next_question in QUESTIONS[self.state]['next_question']:
+      print(f'before state change: {self.state}')
       print(f'input: {input}')
       if input.lower() == next_question['input'].lower():
-        state = next_question['next_question']
-        print(f'after state change: {state}')
+        self.state = next_question['next_question']
+        print(f'after state change: {self.state}')
         print(f'input after: {input}')
         if 'point' in next_question['point']:
           self.score += next_question['point']
           out.append(self.score)
           break
     while True:
-      out.append(QUESTIONS[state]['content'])
-      if 'next_state' not in QUESTIONS[state]:
+      out.append(QUESTIONS[self.state]['content'])
+      if 'next_state' not in QUESTIONS[self.state]:
         break
-      state = QUESTIONS[state]['next_question']
+      self.state = QUESTIONS[self.state]['next_question']
 
     return out
