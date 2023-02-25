33d11a0ec6dfe26eb002737e7db5bb002648e232
Zach <zach.jilani1@gmail.com>
2023-02-24 18:53:48 -0800

    handling wrong input

--- users/player.py
+++ users/player.py
@@ -9,7 +9,6 @@ class Player:
   def __init__(self, number, score=0):
     self.number = number
     self.score = score
-    self.questions_asked = []
     self.state = 'start'
 
   def questions(self, input):
@@ -23,11 +22,12 @@ class Player:
           out.append(f'Score: {self.score}')
           print(out)
         break
+      else:
+        self.state = 'end'
+        out.append(f'Final Score: {self.score}')
     while True:
       out.append(QUESTIONS[self.state]['content'])
       print(QUESTIONS[self.state])
-      if 'next_question' in QUESTIONS[self.state]:
-        print('theres another question')
       if 'next_question' not in QUESTIONS[self.state] or type(QUESTIONS[self.state]['next_question'] != str):
         break
       self.state = QUESTIONS[self.state]['next_question']
