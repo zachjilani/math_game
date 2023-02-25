00f13e9827335021516adc758d7e1e968de2cd4d
Zach <zach.jilani1@gmail.com>
2023-02-24 19:07:33 -0800

    getting wrong input to work properly

--- users/player.py
+++ users/player.py
@@ -20,14 +20,12 @@ class Player:
         if 'point' in next_question:
           self.score += int(next_question['point'])
           out.append(f'Score: {self.score}')
-          print(out)
         break
-      else:
+      if input.lower() != next_question['input'].lower():
         self.state = 'end'
         out.append(f'Final Score: {self.score}')
     while True:
       out.append(QUESTIONS[self.state]['content'])
-      print(QUESTIONS[self.state])
       if 'next_question' not in QUESTIONS[self.state] or type(QUESTIONS[self.state]['next_question'] != str):
         break
       self.state = QUESTIONS[self.state]['next_question']
